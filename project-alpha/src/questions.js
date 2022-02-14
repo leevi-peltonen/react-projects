377a30593bb1efc7f5d7a5bb9d12f94a1ff3cea2
Stetu <leevi.peltonen@icloud.com>
2022-02-14 11:06:11 +0200

    finish

---
+++ project-alpha/src/questions.js
@@ -0,0 +1,62 @@
+const questions = [
+
+    {
+        questionText: 'When was CSGO released?',
+        answerOptions: [
+            { answerText: '2011', isCorrect: false },
+            { answerText: '2012', isCorrect: true },
+            { answerText: '2013', isCorrect: false },
+            { answerText: '2014', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who won the Blast Preimer Final in 2021?',
+        answerOptions: [
+            { answerText: 'Natus Vincere', isCorrect: true },
+            { answerText: 'Gambit', isCorrect: false },
+            { answerText: 'Team Liquid', isCorrect: false },
+            { answerText: 'Vitality', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How much does the PP-Bizon cost?',
+        answerOptions: [
+            { answerText: '2500$', isCorrect: false },
+            { answerText: '1500$', isCorrect: false },
+            { answerText: '1300$', isCorrect: false },
+            { answerText: '1400$', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'How much is the kill award of MAG-7 in competitive mode?',
+        answerOptions: [
+            { answerText: '900$', isCorrect: true },
+            { answerText: '300$', isCorrect: false },
+            { answerText: '600$', isCorrect: false },
+            { answerText: '150$', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who was the 13th best player in CSGO in 2019?',
+        answerOptions: [
+            { answerText: 'Epposan', isCorrect: false },
+            { answerText: 'Stetu', isCorrect: false },
+            { answerText: 'Sergej', isCorrect: true },
+            { answerText: 'Benos', isCorrect: false },
+        ],
+    },
+];
+
+export default questions;
+
+
+// TEMPLATE FOR ADDING NEW QUESTIONS
+// {
+//     questionText: '',
+//     answerOptions: [
+//         { answerText: '', isCorrect: true },
+//         { answerText: '', isCorrect: false },
+//         { answerText: '', isCorrect: false },
+//         { answerText: '', isCorrect: false },
+//     ],
+// },
